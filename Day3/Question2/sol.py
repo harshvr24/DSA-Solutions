c98a2576e3de12bdd0db5be0b3890347d3278932
Harsh Vardhan Katiyar <125513016+harshvr24@users.noreply.github.com>
2024-01-22 22:11:29 +0530

    Create sol.py

---
+++ Day3/Question2/sol.py
@@ -0,0 +1,40 @@
+def majority_elements(nums):
+    if not nums:
+        return []
+
+    candidate1, candidate2, count1, count2 = 0, 1, 0, 0
+
+    # Find candidates using Boyer-Moore Voting Algorithm
+    for num in nums:
+        if num == candidate1:
+            count1 += 1
+        elif num == candidate2:
+            count2 += 1
+        elif count1 == 0:
+            candidate1, count1 = num, 1
+        elif count2 == 0:
+            candidate2, count2 = num, 1
+        else:
+            count1 -= 1
+            count2 -= 1
+
+    # Verify candidates
+    count1, count2 = 0, 0
+    for num in nums:
+        if num == candidate1:
+            count1 += 1
+        elif num == candidate2:
+            count2 += 1
+
+    result = []
+    if count1 > len(nums) // 3:
+        result.append(candidate1)
+    if count2 > len(nums) // 3:
+        result.append(candidate2)
+
+    return result
+
+# Example
+nums = [3, 2, 3]
+result = majority_elements(nums)
+print(result)
