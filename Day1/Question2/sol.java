c26bd99b594c2f93da5184d2ca77b61e6aa83607
Harsh Vardhan Katiyar <125513016+harshvr24@users.noreply.github.com>
2024-01-22 22:02:13 +0530

    Create sol.java

---
+++ Day1/Question2/sol.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class RotateArray {
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;  // To handle cases where k is greater than the array size
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        rotate(nums, k);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
