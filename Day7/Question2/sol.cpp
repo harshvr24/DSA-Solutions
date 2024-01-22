209a3376034eb2b57656b58146e39c15fe965910
Harsh Vardhan Katiyar <125513016+harshvr24@users.noreply.github.com>
2024-01-22 22:26:50 +0530

    Update sol.cpp

--- Day7/Question2/sol.cpp
+++ Day7/Question2/sol.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        
+        int i = 0, t1 = -1, t0 = -1, res = 0;
+        for(int &x : nums) {
+            if(x > right) t1 = i;
+            if(x >= left) t0 = i;
+            
+            res += t0 - t1;
+            ++i;
+        }
+        return res;
+    }
+}
