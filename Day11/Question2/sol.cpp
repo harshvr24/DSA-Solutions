e4dc39b13a5020f20bcf39e84103b745b04839e8
Harsh Vardhan Katiyar <125513016+harshvr24@users.noreply.github.com>
2024-01-28 00:01:20 +0530

    Create sol.cpp

---
+++ Day11/Question2/sol.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+
+        sort(a.begin(), a.end());
+
+        for (int i : a) {
+            cout << i << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
