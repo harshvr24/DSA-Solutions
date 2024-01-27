c85ca96e3bd8b81e76226d4c6b33b228e8c638ea
Harsh Vardhan Katiyar <125513016+harshvr24@users.noreply.github.com>
2024-01-28 00:08:26 +0530

    Create sol.cpp

---
+++ Day12/Question1/sol.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        // Set a1 and a2 to 1, and a3 to n - 2.
+        cout << 1 << " " << 1 << " " << n - 2 << endl;
+    }
+
+    return 0;
+}
