14d46a73ef48188eb008b43cd146669c6e4296f6
Harsh Vardhan Katiyar <125513016+harshvr24@users.noreply.github.com>
2024-02-05 23:58:32 +0530

    Create sol.java

---
+++ Day20/Question2/sol.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String reverseVowels(String s) {
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a'); vowels.add('e'); vowels.add('i'); vowels.add('o'); vowels.add('u');
+        vowels.add('A'); vowels.add('E'); vowels.add('I'); vowels.add('O'); vowels.add('U');
+
+        char[] chars = s.toCharArray();
+        int left = 0, right = chars.length - 1;
+
+        while (left < right) {
+            while (left < right && !vowels.contains(Character.toLowerCase(chars[left]))) {
+                left++;
+            }
+            while (left < right && !vowels.contains(Character.toLowerCase(chars[right]))) {
+                right--;
+            }
+
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(chars);
+    }
+}
