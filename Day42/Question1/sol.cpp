7555f89735bf724c5191e3787ba338bba19fd09b
Harsh Vardhan Katiyar <125513016+harshvr24@users.noreply.github.com>
2024-02-29 00:13:32 +0530

    Update sol.cpp

--- Day42/Question1/sol.cpp
+++ Day42/Question1/sol.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
+    bool isValid(int mid, vector<int>& nums) {
+        return nums[mid] <= nums[nums.size() - 1];
+    }
+
     int findMin(vector<int>& nums) {
-        int n = nums.size();
-        int low=0, high=n-1;
-        
-        while(low<high){
-            if(nums[low] <= nums[high]) return nums[low];
-            int mid = low + (high-low)/2;
-            if(nums[low] > nums[mid]){
-                high=mid;
-            } else if(nums[mid] > nums[high]) {
-                low=mid+1;
-            } 
+        int invalid = -1;
+        int valid = nums.size() - 1;
+
+        while (abs(valid - invalid) > 1) {
+            int mid = (valid + invalid) / 2;
+
+            if (isValid(mid, nums)) {
+                valid = mid;
+            } else {
+                invalid = mid;
+            }
         }
-        if(nums[low] <= nums[high]) return nums[low];
-        return -1;
+
+        return nums[valid];
     }
 };
