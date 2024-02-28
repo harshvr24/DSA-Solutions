ca0133279a93ec69357004f7ee610bddfee1bc70
Harsh Vardhan Katiyar <125513016+harshvr24@users.noreply.github.com>
2024-02-29 00:15:25 +0530

    Create sol.cpp

---
+++ Day42/Question2/sol.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+int findMin(vector<int>& nums) {
+    int low = 0, high = nums.size() - 1;
+
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] > nums[high]) {
+            low = mid + 1;
+        } else if (nums[mid] < nums[high]) {
+            high = mid;
+        } else {
+            high--;
+        }
+    }
+
+    return nums[low];
+}
