2378a66d34222aedef8cb37c02d06b9fbbb36432
Harsh Vardhan Katiyar <125513016+harshvr24@users.noreply.github.com>
2024-02-29 00:32:35 +0530

    Create sol.cpp

---
+++ Day44/Question2/sol.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+
+        while(head) {
+            ListNode* nxt = head->next;
+            head->next = prev;
+            prev = head;
+            head = nxt;
+        }
+        return prev;
+    }
+};
